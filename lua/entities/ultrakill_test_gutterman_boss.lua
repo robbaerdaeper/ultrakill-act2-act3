be502f118de2fa3fc5ef7fc71c9c8545549e2aae
dumbass <124761455+robbaerdaeper@users.noreply.github.com>
2026-07-22 17:03:38 -0400

    gutterman music hell yea baby

--- lua/entities/ultrakill_test_gutterman_boss.lua
+++ lua/entities/ultrakill_test_gutterman_boss.lua
@@ -1,6 +1,7 @@
 AddCSLuaFile()
 local DrGBase = DrGBase
 local UltrakillBase = UltrakillBase
+local BaseClass = baseclass.Get( "ultrakill_test_gutterman_base" )
 
 if not DrGBase or not UltrakillBase then return end
 include( "autorun/ultrakill_test_gutterman_shared.lua" )
@@ -18,3 +19,23 @@ ENT.UKGutterman_Boss = true
 ENT.UKGutterman_Casketless = false
 
 DrGBase.AddNextbot( ENT )
+
+function ENT:OnSpawn()
+
+  BaseClass.OnSpawn( self )
+  
+  self:Timer( 0, UltrakillBase.PlayMusic, "Do Robots Dream of Eternal Sleep?" )
+end
+
+function ENT:OnRemove()
+
+  UltrakillBase.StopCurrentMusic( self )
+
+end
+
+function  ENT:OnDeath()
+
+  BaseClass.OnDeath( self )
+  UltrakillBase.StopCurrentMusic( self )
+
+end
