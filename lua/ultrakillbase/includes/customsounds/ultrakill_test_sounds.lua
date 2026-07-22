30d56e1a06a55dbb3d8a6d1676afb3377864b73e
dumbass <124761455+robbaerdaeper@users.noreply.github.com>
2026-07-22 16:55:58 -0400

    added ultrakill_test_sounds actually i should rename this to ultrakill_test_soundscript

---
+++ lua/ultrakillbase/includes/customsounds/ultrakill_test_sounds.lua
@@ -0,0 +1,18 @@
+-- will convert MOST of the emitsounds into here if possible later
+
+UltrakillBase.AddSoundScript( "Ultrakill_PunchHeavy", "ultrakill/sound/punch_heavy 1.wav", 0, 1, 1, nil, false, 0, false )
+
+UltrakillBase.AddSoundScript( "Ultrakill_GuttermanBonk", "ultrakill_prelude_test/gutterman/bonk.wav", 0, 1, 1, nil, false, 0, false )
+UltrakillBase.AddSoundScript( "Ultrakill_GuttermanDeath", "ultrakill_prelude_test/gutterman/death.wav", 0, 1, 1, nil, false, 0, false )
+UltrakillBase.AddSoundScript( "Ultrakill_GuttermanShieldBreak", "ultrakill_prelude_test/gutterman/shield_break.wav", 0, 1, 1, nil, false, 0, false )
+
+UltrakillBase.AddSoundScript( "Ultrakill_GuttertankShootPrep", "ultrakill_prelude_test/guttertank/rocket_prep.wav", 0, 1, 1, nil, false, 0, false )
+UltrakillBase.AddSoundScript( "Ultrakill_GuttertankShoot", "ultrakill_prelude_test/guttertank/rocket_fire.wav", 0, 1, 1, nil, false, 0, false )
+UltrakillBase.AddSoundScript( "Ultrakill_GuttertankPunchPrep", "ultrakill_prelude_test/guttertank/punch_prep.wav", 0, 1, 1, nil, false, 0, false )
+UltrakillBase.AddSoundScript( "Ultrakill_GuttertankPunchHit", "ultrakill_prelude_test/guttertank/punch_hit.wav", 0, 1, 1, nil, false, 0, false )
+UltrakillBase.AddSoundScript( "Ultrakill_GuttertankMinePrep", "ultrakill_prelude_test/guttertank/mine_prep.wav", 0, 1, 1, nil, false, 0, false )
+UltrakillBase.AddSoundScript( "Ultrakill_GuttertankFallImpact", "ultrakill_prelude_test/guttertank/fall_impact.wav", 0, 1, 1, nil, false, 0, false )
+UltrakillBase.AddSoundScript( "Ultrakill_GuttertankDeath", "ultrakill_prelude_test/guttertank/death.wav", 0, 1, 1, nil, false, 0, false )
+
+UltrakillBase.AddSoundScript( "Ultrakill_VirtueDeath", "ultrakill_test/virtue_death.wav", 0, 1, 1, nil, false, 0, false )
+UltrakillBase.AddSoundScript( "Ultrakill_VirtueCharge", "ultrakill_test/virtue_charge.wav", 0, 1, 1, nil, false, 0, false )
